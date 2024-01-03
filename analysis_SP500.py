5065596cbc8f6b2b03f9e51d04ee0916604159f8
Imanol Irizar <imanol.irizar13@gmail.com>
2024-01-03 17:01:09 +0100

    feature engineering - growths & inflation

--- analysis_SP500.py
+++ analysis_SP500.py
@@ -23,3 +23,58 @@ df = df.loc[is_first_from_year]
 df.loc[:,'Date'] = pd.to_datetime(df.loc[:,'Date']).apply(lambda x: x.year)
 df = df.set_index('Date')
 df
+#%% [Markdown]
+# # Feature Engineering
+#%%
+# Create Yearly Inflation
+inflation_column = 'Inflation'
+cpi_column = 'Consumer Price Index'
+cpi = df.loc[:,cpi_column]
+df.loc[:,inflation_column] = (cpi.diff(1) / cpi * 100).round(2)
+#%%
+# Create SP500 Growth by year span
+growth_spans = list(range(5, 41, 5))
+sp500_growth_column = 'SP500 Growth'
+sp500_column = 'SP500'
+sp500 = df.loc[:,sp500_column]
+df.loc[:,sp500_growth_column] = sp500.rolling(2).apply(lambda x: x.iloc[-1]/x.iloc[0])
+sp500_yearly_growth = df.loc[:,sp500_growth_column]
+df_growth = pd.DataFrame(index=df.index)
+for span in growth_spans:
+    df_growth.loc[:,f'{sp500_growth_column} ({span})'] = (sp500.diff(span) / sp500 * 100).round(2)
+#%%
+# Create SP500 Compounded Growth by year span
+sp500_comp_growth_column = 'SP500 Compounded Growth'
+sp500_growth_yearly_column = 'SP500 Growth'
+sp500_yearly_growth = df.loc[:,sp500_growth_yearly_column]
+df_comp_growth = pd.DataFrame(index=df.index)
+for span in growth_spans:
+    df_comp_growth.loc[:,f'{sp500_comp_growth_column} ({span})'] = sp500_yearly_growth.rolling(span).apply(lambda x: x.product())
+#%%
+import numpy as np
+def calculate_accumulated_growth(yearly_growth: pd.Series, span: int) -> pd.Series:
+    return pd.Series((accumulated_growth(yearly_growth,span,p) for p in range(len(yearly_growth))),index=yearly_growth.index)
+
+def accumulated_growth(yearly_growth: pd.Series, span: int, position: int) -> pd.Series:
+    acc = 0
+    compound_yearly_growth = yearly_growth.iloc[position-span:position].expanding().apply(lambda x: x.product())
+    if compound_yearly_growth.dropna().empty:
+        return np.nan
+    for comp_growth in compound_yearly_growth:
+        if not pd.isnull(comp_growth):
+            acc = acc + 1*comp_growth
+    return acc / len(compound_yearly_growth.dropna())
+        
+#%%
+# Create SP500 Accumulated Growth by year span
+sp500_acc_growth_column = 'SP500 Accumulated Growth'
+sp500_growth_yearly_column = 'SP500 Growth'
+sp500_yearly_growth = df.loc[:,sp500_growth_yearly_column]
+df_acc_growth = pd.DataFrame(index=df.index)
+for span in growth_spans:
+    df_acc_growth.loc[:,f'{sp500_acc_growth_column} ({span})'] = calculate_accumulated_growth(sp500_yearly_growth, span)
+#%%
+# Merge all results
+#%%
+df = pd.concat([df, df_growth, df_comp_growth, df_acc_growth],axis=1)
+#%%
