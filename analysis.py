5b087f8bba12f14541b0f27d7a34d72765b08bb2
imanoliri <imanol.irizar13@gmail.com>
2024-07-19 18:06:41 +0200

    correct to simple inflation, not accumulated

--- analysis.py
+++ analysis.py
@@ -9,6 +9,7 @@ def growth(values: pd.Series, rolling_step: int = 2) -> pd.Series:
 def compound_growth(growths: pd.Series, span: int = 5) -> pd.Series:
     return growths.rolling(span).apply(lambda x: x.product())
 
+
 # TODO: This should be a rolling-apply kind of thing!
 def accumulated_growth(yearly_growth: pd.Series, span: int) -> pd.Series:
     return pd.Series((acc_growth_point(yearly_growth,span,p) for p in range(len(yearly_growth))),index=yearly_growth.index)
@@ -25,35 +26,44 @@ def acc_growth_point(yearly_growth: pd.Series, span: int, position: int) -> pd.S
     return acc / len(compound_yearly_growth.dropna())
 
 
-def add_growth_infos(df: pd.DataFrame, value_column: str, other_earning_columns: List[str] = None, *, range_start: int = 5, range_end: int = 41, range_step = 5, real_value: bool = False) -> pd.DataFrame:
-    value_str = value_column.lower().replace(' ', '_')
-    real_str = 'real' if real_value else 'nominal'
-    new_value_column = (value_str, real_str, 'value', '')
-    growth_column = (value_str, real_str, 'growth', '')
-    comp_growth_column = (value_str, real_str, 'compound_growth')
-    acc_growth_column = (value_str, real_str, 'accumulated_growth')
+def generate_growth_infos(df: pd.DataFrame, *, value_column: str=None, growth_column: str=None, range_start: int = 5, range_end: int = 41, range_step = 5) -> pd.DataFrame:
     growth_spans = list(range(range_start, range_end, range_step))
+    comp_growth_column ='compound_growth'
+    acc_growth_column = 'accumulated_growth'
+    
+    # Get growths
+    if value_column and not growth_column:
+        values = df.loc[:,value_column]
+        growths = growth(values)
+    elif not value_column and growth_column:
+        growths = df.loc[:,growth_column]
+    else:
+        raise ValueError('One value column XOR growth column must be defined.')
 
-    df_growth = pd.DataFrame()
 
-    # Values
-    values = df.loc[:,value_column]
-    if other_earning_columns is not None:
-        for o_col in other_earning_columns:
-            values = values + df.loc[:, o_col]
-
-    # Yearly growths
-    growths = growth(values)
-
-    df_growth = pd.DataFrame([values.values, growths.values], index = pd.MultiIndex.from_tuples([new_value_column, growth_column]), columns=df.index).T
-
+    df_growth = pd.DataFrame(index=growths.index,columns=pd.MultiIndex.from_product([(comp_growth_column,acc_growth_column),growth_spans]))
     # Compound growths for the defined spans
     for span in growth_spans:
-        df_growth.loc[:, (*comp_growth_column, span)] = compound_growth(growths, span)
+        df_growth.loc[:, (comp_growth_column, span)] = compound_growth(growths, span)
     
     # Accumulated growths (as if you had invested 1 unit over the last years in the span and each year's had grown with it's own compounded interest) for the defined spans
     for span in growth_spans:
-        df_growth.loc[:, (*acc_growth_column, span)] = accumulated_growth(growths, span)
+        df_growth.loc[:, (acc_growth_column, span)] = accumulated_growth(growths, span)
+
+    return df_growth
+
+
+
+def generate_value_growth_infos(df: pd.DataFrame, *, value_column: str, range_start: int = 5, range_end: int = 41, range_step = 5) -> pd.DataFrame:
+    growth_spans = list(range(range_start, range_end, range_step))
+    growth_column ='growth'    
+
+    values = df.loc[:,value_column]
+    df_growth = pd.DataFrame(index=values.index,columns=pd.MultiIndex.from_product([[growth_column],growth_spans]))
+    
+    # Get value growths for the defined spans
+    for span in growth_spans:
+        df_growth.loc[:, (growth_column, span)] = growth(values, span)
 
     return df_growth
 
