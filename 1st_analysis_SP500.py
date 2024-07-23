51a145863668aec7b5cf294c90f50b116b84ce79
imanoliri <imanol.irizar13@gmail.com>
2024-07-23 06:50:41 +0200

    correct to compound growth

--- 1st_analysis_SP500.py
+++ 1st_analysis_SP500.py
@@ -97,7 +97,7 @@ df_new.describe().round(2).to_csv(f'{results_dir}/sp500_new_describe.csv')
 from plot import combined_plot, multiplot, overlap_plot
 selected_index = 'sp500'
 selected_strategy = ('sp500', 'dca_compound_growth', 40)
-selected_retirements = [(f'sp500_minus_{rdraw}_percent', 'dca_compound_growth', 40) for rdraw in retirement_draws]
+selected_retirements = [(f'sp500_minus_{rdraw}_percent', 'compound_growth', 40) for rdraw in retirement_draws]
 selected_inflation = ('prices', 'growth', 40)
 
 sp500_value_series = ((selected_index, 'line', 'tab:orange', dict(logy=True)),)
